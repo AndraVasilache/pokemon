3c5cbffe427e3560c5d672a57b7b97e3b7935a49
AndraVasilache <vasilacheandragabriela@gmail.com>
2021-07-02 09:30:38 +0300

    feat: add create method for Trainer model

--- src/main/java/com/pokemon/PokemonProjectApplication.java
+++ src/main/java/com/pokemon/PokemonProjectApplication.java
@@ -3,12 +3,13 @@ package com.pokemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableJpaRepositories
+@SpringBootApplication
 public class PokemonProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonProjectApplication.class, args);
 	}
-
 }
